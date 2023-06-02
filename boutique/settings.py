76bbf39a88310b411c25795fdbe204d5ee41ffc8
bethanyalicehall <bethany.hall66@gmail.com>
2023-06-02 09:27:49 +0000

    Added shopping bag page and free delivery threshold logic

--- boutique/settings.py
+++ boutique/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g=-aq#-%o%(_r+jibzc^d&o5=ls-k%pllkz^0amp3tpva+blcs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '8000-bethanyaliceha-boutique-r8uursjzgae.ws-eu98.gitpod.io']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -153,6 +155,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
